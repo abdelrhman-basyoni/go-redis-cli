cf02c512341e81662c63d89896514b636abcf14f
Abdelrhman Basyoni <64443028+abdelrhman-basyoni@users.noreply.github.com>
2024-12-09 15:52:00 +0200

    updating the cli  to work with the new resp

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/abdelrhman-basyoni/gorediscli
 
 go 1.22.4
 
-require github.com/abdelrhman-basyoni/goresp v0.1.2
+require github.com/abdelrhman-basyoni/goresp v0.1.5
--- main.go
+++ main.go
@@ -33,6 +33,7 @@ func main() {
 	scanner := bufio.NewScanner(os.Stdin)
 
 	for {
+		fmt.Println()
 		fmt.Print("redis> ")
 		if !scanner.Scan() {
 			break
@@ -50,61 +51,19 @@ func main() {
 		}
 
 		// Send the command to Redis server
-		_, err := conn.Write(serializeCommand(line))
+		_, err := conn.Write(goresp.SerializeCommand(line))
 		if err != nil {
 			fmt.Printf("Error writing to Redis server: %v\n", err)
 			break
 		}
 
 		// Read the response from Redis server
-		// response, err := bufio.NewReader(conn).ReadString('\n')
 		resReader := goresp.NewRespReader(conn)
-		// if err != nil {
-		// 	fmt.Printf("Error reading response from Redis server: %v\n", err)
-		// 	break
-		// }
-
 		res, err := resReader.Read()
 		if err != nil {
 			fmt.Printf("Error reading  from server: %v\n", err)
 			break
 		}
-		fmt.Println("response from Redis server")
-		fmt.Println(serializeReaderCommand(res))
-	}
-}
-
-// serializeCommand converts the command into RESP (REdis Serialization Protocol) format
-func serializeCommand(command string) []byte {
-	parts := strings.Fields(command)
-
-	var test []goresp.Value
-
-	for _, part := range parts {
-
-		bulk := goresp.Value{Typ: "bulk", Bulk: part}
-		test = append(test, bulk)
-	}
-
-	newCommand := goresp.Value{Typ: "array", Array: test}
-
-	fmt.Println(len(newCommand.Array))
-	fmt.Println(newCommand)
-	fmt.Println(newCommand.Marshal())
-	return newCommand.Marshal()
-}
-
-func serializeReaderCommand(res goresp.Value) string {
-
-	switch res.Typ {
-	case "bulk":
-		{
-			return res.Bulk
-		}
-
-	default:
-		{
-			return res.Bulk
-		}
+		fmt.Println(goresp.SerializeReaderCommand(res))
 	}
 }
